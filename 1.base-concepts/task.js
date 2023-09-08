fdc7de2d66fd88f428bc1c5144e35c22dd00e0d1
Landysh Sagitova <landysh.sagitova@mail.ru>
2023-09-08 15:45:01 +0300

    Первое задание с учетом замечаний

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -8,18 +8,18 @@ function solveEquation(a, b, c) {
 	let rootOfEquation2 = (-b - Math.sqrt(discriminant)) / (2 * a);
 	arr.push(rootOfEquation1,rootOfEquation2)
   } else if (discriminant === 0) {
-		let rootOfEquation3 = -b / (2 * a);
-		arr.push(rootOfEquation3);
+	  let rootOfEquation3 = -b / (2 * a);
+	  arr.push(rootOfEquation3);
   };
   return arr;
 }
 
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-	let percentMonth = (parseInt(percent) / 100) / 12;
-	let amountReturnedBank = parseInt(amount) - parseInt(contribution);
-	let numberMonths = parseInt(countMonths);
-	let payment = amountReturnedBank * (percentMonth + (percentMonth / (((1 + percentMonth) ** numberMonths) - 1)));
-	let totalAmount = payment * numberMonths;
-	return +totalAmount.toFixed(2);
+  let percentMonth = (parseInt(percent) / 100) / 12;
+  let amountReturnedBank = parseInt(amount) - parseInt(contribution);
+  let numberMonths = parseInt(countMonths);
+  let payment = amountReturnedBank * (percentMonth + (percentMonth / (((1 + percentMonth) ** numberMonths) - 1)));
+  let totalAmount = payment * numberMonths;
+  return +totalAmount.toFixed(2);
 };
