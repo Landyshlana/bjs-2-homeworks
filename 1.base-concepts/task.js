7e92bd5a5fa3d67821ad1b0fb365abc011023712
Landysh Sagitova <landysh.sagitova@mail.ru>
2023-09-08 15:31:16 +0300

    Первое задание с учетом замечаний

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,38 +1,25 @@
 "use strict"
 
 function solveEquation(a, b, c) {
-	let arr = [];
-	let i;
-	let j;
-	let d = Math.pow(b, 2) - 4 * a * c;
-	if (d > 0) {
-		i = (-b + Math.sqrt(d)) / (2 * a);
-		j = (-b - Math.sqrt(d)) / (2 * a);
-		arr.push(i) && arr.push(j)
-	} else
-	if (d < 0) {
-		arr = [];
-	} else
-	if (d === 0) {
-		i = -b / (2 * a);
-		arr.push(i);
-	};
-	return arr;
+  let arr = [];
+  let discriminant = Math.pow(b, 2) - 4 * a * c;
+  if (discriminant > 0) {
+	let rootOfEquation1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+	let rootOfEquation2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+	arr.push(rootOfEquation1,rootOfEquation2)
+  } else if (discriminant === 0) {
+		let rootOfEquation3 = -b / (2 * a);
+		arr.push(rootOfEquation3);
+  };
+  return arr;
 }
 
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-	if (isNaN(parseInt(percent)) === true || isNaN(parseInt(contribution)) === true || isNaN(parseInt(amount)) === true ||
-		isNaN(parseInt(countMonths)) === true ||
-		parseInt(percent) === 0 || parseInt(amount) === 0 ||
-		parseInt(countMonths) === 0
-	) {
-		return false;
-	}
-	let P = (parseInt(percent) / 100) / 12;
-	let S = parseInt(amount) - parseInt(contribution);
-	let n = parseInt(countMonths);
-	let payment = S * (P + (P / (((1 + P) ** n) - 1)));
-	let totalAmount = payment * n;
+	let percentMonth = (parseInt(percent) / 100) / 12;
+	let amountReturnedBank = parseInt(amount) - parseInt(contribution);
+	let numberMonths = parseInt(countMonths);
+	let payment = amountReturnedBank * (percentMonth + (percentMonth / (((1 + percentMonth) ** numberMonths) - 1)));
+	let totalAmount = payment * numberMonths;
 	return +totalAmount.toFixed(2);
 };
