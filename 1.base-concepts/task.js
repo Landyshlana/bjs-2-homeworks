7b1005842c0b4160da9796ca4407573febdf9e14
Landysh Sagitova <landysh.sagitova@mail.ru>
2023-09-08 15:48:27 +0300

    Первое задание с учетом замечаний

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,9 +4,9 @@ function solveEquation(a, b, c) {
   let arr = [];
   let discriminant = Math.pow(b, 2) - 4 * a * c;
   if (discriminant > 0) {
-	let rootOfEquation1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-	let rootOfEquation2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-	arr.push(rootOfEquation1,rootOfEquation2)
+	  let rootOfEquation1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+	  let rootOfEquation2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+	  arr.push(rootOfEquation1,rootOfEquation2);
   } else if (discriminant === 0) {
 	  let rootOfEquation3 = -b / (2 * a);
 	  arr.push(rootOfEquation3);
