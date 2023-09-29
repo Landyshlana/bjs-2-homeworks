577f354961e0fbc8e05f512cd1e2735e31030432
Landysh Sagitova <landysh.sagitova@mail.ru>
2023-09-29 20:39:53 +0300

    четвёртое задание готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,43 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
-
-Student.prototype.setSubject = function (subjectName) {
-  
+let student1 = new Student("Василиса", "женский", 19);
+let student2 = new Student("Иван", "мужской", 20);
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName
 }
-
-Student.prototype.addMarks = function (...marks) {
-  
+student1.setSubject("a");
+student2.setSubject("b");
+Student.prototype.addMarks = function(...tooAddmarks) {
+	if ('marks' in this) {
+		this.marks.push(...tooAddmarks)
+	}
 }
-
-Student.prototype.getAverage = function () {
-  
+student1.addMarks(5, 5);
+student2.addMarks(3, 3, 3);
+console.log(student1);
+console.log(student2);
+Student.prototype.getAverage = function() {
+	if ("marks" in this === false || this.marks.length === 0) {
+		return 0;
+	}
+	let sum = 0;
+	for (let i = 0; i < this.marks.length; i++) {
+		sum += this.marks[i];
+	}
+	return sum / this.marks.length
+};
+console.log(student1.getAverage());
+console.log(student2.getAverage());
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
 }
-
-Student.prototype.exclude = function (reason) {
-  
-}
+student2.exclude('неуд')
+student2.addMarks(3, 3, 3);
+console.log(student2);
+console.log(student2.getAverage());
