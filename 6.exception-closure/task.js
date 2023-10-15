0bbd6602d24cf7a406270ace8b5a591ac273c061
Landysh Sagitova <landysh.sagitova@mail.ru>
2023-10-15 17:16:51 +0300

    шестое задание

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,47 @@
-﻿
+﻿function parseCount(count) {
+	if (isNaN(Number.parseFloat(count))) {
+		throw new Error("Невалидное значение");
+	}
+	return Number.parseFloat(count);
+}
+
+function validateCount(count) {
+	try {
+		if (!isNaN(parseCount(count))) {
+			return (parseCount(count))
+		}
+	} catch (error) {
+		return (error);
+	}
+}
+class Triangle {
+	constructor(a, b, c) {
+		if (a + b <= c || a + c <= b || c + b <= a) {
+			throw new Error("Треугольник с такими сторонами не существует")
+		};
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+	get perimeter() {
+		return (this.a + this.b + this.c);
+	}
+	get area() {
+		return +((Math.sqrt((this.perimeter / 2) * (this.perimeter / 2 - this.a) * (this.perimeter / 2 - this.b) * (this.perimeter / 2 - this.c))).toFixed(3));
+	}
+}
+const getTriangle = (a, b, c) => {
+	try {
+		const triangle = new Triangle(a, b, c);
+		return triangle;
+	} catch (error) {
+		return {
+			get perimeter() {
+				return "Ошибка! Треугольник не существует"
+			},
+			get area() {
+				return "Ошибка! Треугольник не существует"
+			}
+		}
+	}
+}
